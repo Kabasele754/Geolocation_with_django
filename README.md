dda1fa816d0ad8286e8004dba6132919e66705ab
Achille kabasele <pepexykabasele@gmail.com>
2022-10-29 12:20:30 +0200

    add function class to get api data

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Geolocation_with_django
+
+# Site for to get api:
+
+## 1 site https://www.ipify.org/ for ip 
+
+### https://api.ipify.org?format=json
+
+## 2 Site https://ip-api.com/docs/api:json for geolocation
+ 
+### api http://ip-api.com/json/24.48.0.1
--- app_geo/views.py
+++ app_geo/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+import requests
+import json
 from django.views.generic import TemplateView
 
 # Create your views here.
 
 class Home(TemplateView):
     template_name= "geo/index.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        ip = requests.get('https://api.ipify.org?format=json')
+        ip_data = json.loads(ip.text)
+        res = requests.get('http://ip-api.com/json/24.48.0.1')#+ip_data['ip']
+        location_data_one = res.text
+        location_data = json.loads(location_data_one)
+        context['data'] = location_data
+        return context
